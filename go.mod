285785b9406d676590ed3e072a45a12ee06f9a52
francisco-serrano <francisco.serrano372@gmail.com>
2020-12-08 04:24:06 -0300

    adds fiber

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/francisco-serrano/sample-gokit
 
 go 1.13
 
-require github.com/go-kit/kit v0.10.0
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/gofiber/adaptor/v2 v2.1.1
+	github.com/gofiber/fiber/v2 v2.2.3
+	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
+)
--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"github.com/francisco-serrano/sample-gokit/service"
 	"github.com/francisco-serrano/sample-gokit/transport"
 	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
 	"log"
-	"net/http"
 )
 
 func main() {
@@ -17,7 +18,10 @@ func main() {
 		transport.EncodeResponse,
 	)
 
-	http.Handle("/hello", helloHandler)
+	app := fiber.New()
+	app.Get("/hello", adaptor.HTTPHandler(helloHandler))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
